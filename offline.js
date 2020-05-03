40f700101ac3915b93cc1e4abec5532298343aad
diyar-2020 <dyartymwry1@gmail.com>
2020-05-03 14:05:17 +0430

    22222

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1588498504,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"g_bg.png",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite3-sheet0.png",
+		"logo-sheet0.png",
+		"bg_l.png",
+		"sprite4-sheet0.png",
+		"sprite5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
